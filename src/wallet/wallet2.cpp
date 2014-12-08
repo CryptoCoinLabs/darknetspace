08f5f43ba5358565012ded2b94f70bd357fec5c8
CryptoCoinLab <support@CryptoCoinLab.com>
2014-12-08 15:57:26 +0800

    Fix simplewallet can't refresh money.
    Fix simplewallet can't refresh money.

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -448,7 +448,7 @@ void wallet2::refresh(size_t & blocks_fetched, bool& received_money)
       }
       else
       {
-        LOG_ERROR("pull_blocks failed, try_count = " << try_count << ", try next time when called...);
+        LOG_ERROR("pull_blocks failed, try_count = " << try_count << ", try next time when called...");
         //throw;
       }
     }
